7c3cd4073a09f973d0d15b1d649888b4586d6010
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-09-27 00:49:35 +0530

    Initial commit
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- website/login_module/views.py
+++ website/login_module/views.py
@@ -1,24 +1,3 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .helper import *
 
-def login(request):
-	context = {
-		'error' : None,
-	}
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		error = None
-		if user_exists(username):
-			valid_user = valid_credentials(username, password)
-			if valid_user:
-				userid = valid_user.id
-				return redirect(user, userid)
-			else:
-				error = 'Invalid password'
-		else:
-			error = 'Invalid username'
-		context['error'] = error
-		return render(request, 'login.html', context)
-	return render(request, 'login.html', context)
