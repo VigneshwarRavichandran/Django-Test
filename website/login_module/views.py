a1e9e0634bc9e664b18119b6193f8bd5b4cd43a1
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-10-09 22:27:38 +0530

    Register Features
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- website/login_module/views.py
+++ website/login_module/views.py
@@ -22,3 +22,19 @@ def login(request):
 		context['error'] = error
 		return render(request, 'login.html', context)
 	return render(request, 'login.html', context)
+
+def register(request):
+	context = {
+		'error' : None,
+	}
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		firstname = request.POST.get('firstname')
+		lastname = request.POST.get('lastname')
+		if not user_exists(username):
+			create_user(username, password, firstname, lastname)
+			return redirect(login)
+		context['error'] = 'Username already exists'
+		return render(request, 'register.html', context)
+	return render(request, 'register.html', context)
